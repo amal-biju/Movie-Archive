18acbccf8de61ab7dbb66817c770c017878df46c
Amal Biju <amalbiju99@gmail.com>
2021-02-25 19:50:47 +0530

    Updated API url

--- movie.js
+++ movie.js
@@ -1,32 +1,30 @@
 window.addEventListener("load", () => {
-    let query = window.location.search;
-    loadQuery( query )
-})
+   let query = window.location.search;
+   loadQuery(query);
+});
 
-
-function loadQuery( query ){
-    let url = new URLSearchParams( query )
-    let imdbID = url.get("id")
-    // document.body.append(imdbID)
-    getMovieDetails(imdbID)
+function loadQuery(query) {
+   let url = new URLSearchParams(query);
+   let imdbID = url.get("id");
+   // document.body.append(imdbID)
+   getMovieDetails(imdbID);
 }
 
-function getMovieDetails(input){
-    const xhr = new XMLHttpRequest()
+function getMovieDetails(input) {
+   const xhr = new XMLHttpRequest();
 
-    xhr.open("GET",`http://www.omdbapi.com/?apikey=e49831aa&i=${input}`)
-    xhr.send();
+   xhr.open("GET", `https://www.omdbapi.com/?apikey=e49831aa&i=${input}`);
+   xhr.send();
 
-    xhr.onload = function(){
-        var res = JSON.parse(this.response)
-        console.log(res);
-        displayMovieDetails(res)
-    }
+   xhr.onload = function () {
+      var res = JSON.parse(this.response);
+      displayMovieDetails(res);
+   };
 }
 
-function displayMovieDetails(movie){
-    const container = document.getElementById('container');
-    container.innerHTML = `
+function displayMovieDetails(movie) {
+   const container = document.getElementById("container");
+   container.innerHTML = `
         <div id="imageWrapper">
             <img src="${movie.Poster}"/>
         </div>
@@ -77,5 +75,5 @@ function displayMovieDetails(movie){
                 <a href ="https://www.imdb.com/title/${movie.imdbID}/" target="_blank"><button>View IMDb</button></a>
                 <a href="index.html"><button>Back To Search</button></a>
             </section>
-        </div>`
+        </div>`;
 }
